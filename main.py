b556b33ff5b10407a1f20d0a8e0b10aa8b356312
Edoardo Regni <eregni@pm.me>
2022-11-24 15:37:34 +0100

    added ports & todo's

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+#TODO: add protocol + port + TCP|UDP
+#TODO: Mix it all ports <-> nrs
 import re
 from random import shuffle
 
@@ -9,7 +11,8 @@ DATA = [
     ("23", "TELNET", "(Remote connection without encryption)"),
     ("25", "SMTP", "(Simple Mail Transfer Protocol)"),
     ("53", "DNS", "(Domain Name System)"),
-    ("69", "TFPT" "(Trivial File Transfer Protocol)"),
+    ("67", "DHCP", "(Dynamic host control protocol)"),
+    ("69", "TFPT", "(Trivial File Transfer Protocol)"),
     ("80", "HTTP", "(Hypertext Transfer Protocol)"),
     ("110", "POP3", "(Post Office Protocol)"),
     ("123", "NTP", "(Network Time Protocol)"),
@@ -26,21 +29,18 @@ DATA = [
     ("993", "IMAPS", "(Secure IMAP)"),
     ("995", "POP3S", "(Secure POP3)")
 ]
-shuffle(DATA)
-
 
 def play_ports(data: list[tuple]) -> None:
-    correct = 0
+    shuffle(DATA)
     wrong = []
     for item in data:
         answer = input(f"Give port for {item[1]} {item[2]}: ")
-        if answer == item[0]:
-            correct += 1
-        else:
+        if answer != item[0]:
             wrong.append((answer, item))
-
+    
+    correct = len(data) - len(wrong)
     print(f"\nScore: {correct}/{len(data)}")
-    if correct == len(data):
+    if len(wrong) == 0:
         print("Perfect!")
     else:
         print("\nThese answers where incorrect:")
@@ -49,26 +49,26 @@ def play_ports(data: list[tuple]) -> None:
 
 
 def play_protocols(data: list[tuple]) -> None:
-    correct = 0
+    shuffle(DATA)
     wrong = []
     for item in data:
         answer = input(f"Give protocol for port {item[0]}: ")
         re_pattern = re.compile('[ ()]')
         _answer = re.sub(re_pattern, '', answer).lower()
         right_answer = re.sub(re_pattern, '', item[1]).lower()
-        if _answer == right_answer:
-            correct += 1
-        else:
+        if _answer != right_answer:
             wrong.append((answer, item))
-
+           
     print(f"\nScore: {correct}/{len(data)}")
-    if correct == len(data):
+    if len(wrong) == 0:
         print("Perfect!")
     else:
         print("\nThese answers where incorrect:")
         for item in wrong:
             print(f"Port {item[1][0]}: You answered '{item[0]}'. Right answer -> {item[1][1]}")
 
+def evaluation():
+    Raise NotImplentedError
 
 print("#########################")
 print("# Port & Protocol quiz! #")
@@ -76,12 +76,13 @@ print("#########################\n")
 print("Ports: Give the corresponding port for given protocol:")
 print("Protocols: Give the corresponding protocol for given port:")
 
-while True:
-    print("\nSelect quiz")
-    choice = input("Ports(1) of Protocols(2) or quit(q): ")
-    if choice in ["Q", "q"]:
-        exit(0)
-    elif choice == "1":
-        play_ports(DATA)
-    elif choice == "2":
-        play_protocols(DATA)
+if __name__ == "__main__":
+    while True:
+        print("\nSelect quiz")
+        choice = input("Ports(1) of Protocols(2) or quit(q): ")
+        if choice in ["Q", "q"]:
+            exit(0)
+        elif choice == "1":
+            play_ports(DATA)
+        elif choice == "2":
+            play_protocols(DATA)
